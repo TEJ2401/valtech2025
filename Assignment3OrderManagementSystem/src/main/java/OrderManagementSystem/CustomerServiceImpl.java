c75a68028c6f8b6c8cfaf9239dd2b793358e5aaa
TEJ2401 <105072288+TEJ2401@users.noreply.github.com>
2025-04-05 20:31:35 +0530

    Update CustomerServiceImpl.java

--- Assignment3OrderManagementSystem/src/main/java/OrderManagementSystem/CustomerServiceImpl.java
+++ Assignment3OrderManagementSystem/src/main/java/OrderManagementSystem/CustomerServiceImpl.java
@@ -1,4 +1,4 @@
-package spring.OrderManagementSystem;
+package OrderManagementSystem;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
